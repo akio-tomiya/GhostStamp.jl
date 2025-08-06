0cd0b66dbb23b4397325daa74797817f72b53f32
Akio TOMIYA <akio.tomiya@hotmail.com>
2025-08-06 14:10:04 -0400

    Initial commit

--- src/GhostStamp.jl
+++ src/GhostStamp.jl
@@ -1,5 +1,30 @@
-module GhostStamp
+module WatermarkPlots
 
-# Write your package code here.
+using Plots
+using Colors # Colorant, RGBA
+using Statistics: mean      # xmid, ymid
 
+export add_preliminary!,add_stamp!     # Public API
+
+"""
+    add_preliminary!(plt; txt="PRELIMINARY", angle=30, fs=40,
+                     color=:red, alpha=0.15)
+
+A helper to add a watermark for a figure (`plt`) by Plots.jl 
+"""
+function add_preliminary!(plt; text="PRELIMINARY", angle=30,
+    fs=40, color=:red, alpha=0.15)
+    add_stamp!(plt; text=text, angle=angle,fs=fs, color=color, alpha=alpha)
 end
+function add_stamp!(plt; text="", angle=30,
+                          fs=40, color=:red, alpha=0.15)
+
+    base = color isa Colorant ? color : parse(Colorant, string(color))
+    txtcol = RGBA(red(base), green(base), blue(base), alpha)
+    t = text(text, fs, txtcol; halign=:center, valign=:center, rotation=angle)
+
+    xmid = mean(xlims(plt));  ymid = mean(ylims(plt))
+    annotate!(plt, (xmid, ymid, t))
+end
+
+end # module
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,6 +1,14 @@
-using GhostStamp
+using GhostStamp,Plots
 using Test
 
 @testset "GhostStamp.jl" begin
-    # Write your tests here.
+
+    plt = plot(1:10, 1:10)
+    add_preliminary!(plt)
+    @test length(Plots.annotations(plt)) == 1
+
+    plt = plot(1:10, 1:10)
+    add_stamp!(plt; txt="TEST")
+    @test length(Plots.annotations(plt)) == 1
+
 end
